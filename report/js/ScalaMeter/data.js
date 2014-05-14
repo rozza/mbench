99f3c11f5d967fa7a8bdc245d977c2323597955e
Ross Lawley <ross.lawley@gmail.com>
2014-05-14 18:03:14 +0100

    Updates

--- report/js/ScalaMeter/data.js
+++ report/js/ScalaMeter/data.js
@@ -1,6 +1,6 @@
 var ScalaMeter = (function(parent) {
   var my = { name: "data" };
-  my.index = [{"scope" : ["Insert", "MongoDB inserts simple documents"], "name" : "Sync", "file" : "..\/Insert.MongoDB inserts simple documents.Sync.dsv"}, {"scope" : ["Insert", "MongoDB inserts simple documents"], "name" : "Async", "file" : "..\/Insert.MongoDB inserts simple documents.Async.dsv"}, {"scope" : ["Insert", "mongodb-driver-async"], "name" : "Sync", "file" : "..\/Insert.mongodb-driver-async.Sync.dsv"}, {"scope" : ["Insert", "mongodb-driver-async"], "name" : "Async", "file" : "..\/Insert.mongodb-driver-async.Async.dsv"}];
+  my.index = [{"scope" : ["Single Inserts", "3.0.x Driver"], "name" : "Sync", "file" : "..\/Single Inserts.3.0.x Driver.Sync.dsv"}, {"scope" : ["Single Inserts", "3.0.x Driver"], "name" : "Async", "file" : "..\/Single Inserts.3.0.x Driver.Async.dsv"}, {"scope" : ["Single Inserts", "mongodb-driver-async"], "name" : "Sync", "file" : "..\/Single Inserts.mongodb-driver-async.Sync.dsv"}, {"scope" : ["Single Inserts", "mongodb-driver-async"], "name" : "Async", "file" : "..\/Single Inserts.mongodb-driver-async.Async.dsv"}];
   parent[my.name] = my;
   return parent;
 })(ScalaMeter || {});
