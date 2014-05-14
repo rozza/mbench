2b89375d4f7bb71a0789a882aa1be981de573374
Ross Lawley <ross.lawley@gmail.com>
2014-05-14 08:17:34 +0100

    Report

---
+++ report/js/ScalaMeter/data.js
@@ -0,0 +1,6 @@
+var ScalaMeter = (function(parent) {
+  var my = { name: "data" };
+  my.index = [{"scope" : ["Insert", "MongoDB inserts simple documents"], "name" : "Sync", "file" : "..\/Insert.MongoDB inserts simple documents.Sync.dsv"}, {"scope" : ["Insert", "MongoDB inserts simple documents"], "name" : "Async", "file" : "..\/Insert.MongoDB inserts simple documents.Async.dsv"}, {"scope" : ["Insert", "mongodb-driver-async"], "name" : "Sync", "file" : "..\/Insert.mongodb-driver-async.Sync.dsv"}, {"scope" : ["Insert", "mongodb-driver-async"], "name" : "Async", "file" : "..\/Insert.mongodb-driver-async.Async.dsv"}];
+  parent[my.name] = my;
+  return parent;
+})(ScalaMeter || {});
